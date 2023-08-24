a07cb1fd4130593248d03e5586d2788413d7feed
joel magana <joelmagana15@gmail.com>
2023-08-24 01:17:55 -0700

    montygit add .

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top
+ * @head: the stack head
+ * @counter: line_number
+ * Return: nothing
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
